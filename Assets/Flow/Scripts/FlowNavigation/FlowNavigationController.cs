2d0e3875076fff027643d953c00cd8964d28974a
davidkimighty <davidkimighty@gmail.com>
2025-07-25 15:24:25 +0900

    project cleanup

---
+++ Assets/Flow/Scripts/FlowNavigation/FlowNavigationController.cs
@@ -0,0 +1,14 @@
+using boop;
+using UnityEngine;
+
+namespace Gallery
+{
+    public class FlowNavigationController : MonoBehaviour
+    {
+        [SerializeField] private Button _selectButton;
+        [SerializeField] private Button _backButton;
+        [SerializeField] private Button _upButton;
+        [SerializeField] private Button _downButton;
+
+    }
+}
